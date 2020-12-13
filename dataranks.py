c96c2edc6b2372966219003817bbe451479175d4
noverd <dasfasru@gmail.com>
2020-12-13 21:47:36 +0300

    Обнова записи даты пользователей добавил сохранение даты пользователев файл

---
+++ dataranks.py
@@ -0,0 +1,4 @@
+r = [
+{'ID': 720879793645551697, 'name': 'Jaйtom#1441', 'rank': 4, 'server_name': 'tomjas', 'server_id': '783319350563700746', 'member_count': '2'},
+{'ID': 720879793645551697, 'name': 'Jaйtom#1441', 'rank': 1, 'server_name': 'Сервер Пайtom', 'server_id': '783319282024054786', 'member_count': '2'},
+]
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import discord
+import os
 import time
+from dataranks import r as ranks
 
 TOKEN = 'NzgzMzE4NDMyODMyNTUyOTgy.X8ZAIw.ZLvTDoEDl89U3x32NZfc2JDBEYY'
 
 mat = ("сук", 'xуй', "пидр", "бля", "го")
-
-ranks = [{"ID": 776096588048367646, "name": "tester100#3585", "rank": 0, "server_name": "GG3's server",
-          "server_id": "710860566184329268", "member_count": 4}]
-
 
 
 class XCLient(discord.Client):
@@ -16,6 +14,8 @@ class XCLient(discord.Client):
         print(f"ID bot:{self.user.id}")
 
     async def on_message(self, message):
+        f = open("dataranks.py", "w")
+        f.close()
         user_name = str(message.author)
         user = message.author
         ID = user.id
@@ -36,40 +36,6 @@ class XCLient(discord.Client):
         print("Количество членов сервера:" + str(guild.member_count))
         print(ranks)
 
-        for i, s in enumerate(ranks):
-            print(s["ID"], ' == ', ID, s['ID'] == ID)
-            if s["ID"] == ID and s["server_id"] == str(guild.id) and not args.startswith("!"):
-                print(s)
-                print(ranks[i]['rank'])
-                ranks[i]['rank'] += 1
-                print(ranks[i]['rank'])
-                k = 1
-        if k == 0:
-            ranks.append(
-                {"ID": ID, 'name': user_name, 'rank': 1, "server_name": str(guild.name),
-                 "server_id": str(guild.id), "member_count": str(guild.member_count)})
-
-        for s in ranks:
-            print(s)
-
-        for x in mat:
-            if x in args.lower():
-                await chan.send("не матерись")
-                time.sleep(2)
-                await discord.Guild.ban(guild, user=user, reason="Ты плахая папапо", delete_message_days=1)
-                embed = discord.Embed(color=0xFF7C00,
-                                      title="Пользователь " + f"@{user} был забанен на 24 часа.Причана - маты")
-                embed.set_image(
-                    url="https://cdn.discordapp.com/attachments/768082564019126292/782184876102254592/489d22e04e9ea5f8.png")
-                await chan.send(embed=embed)
-                time.sleep(86400)
-                embed = discord.Embed(color=0xFF7C00, title="Пользователь " + f"@{user} Разбанен.Возврощайся")
-                embed.set_image(
-                    url="https://cdn.discordapp.com/attachments/768082564019126292/782222100780810250/2017-01-06-11-04-1982b2d7e556bdb895daece3edfa0789.jpg")
-                await chan.send(embed=embed)
-                await discord.Guild.unban(guild, user=user, reason="Ты плахая папапо")
-                return
-
         if args.startswith('!'):
             commands_bot = args.split()
             if args.startswith("!rank"):
@@ -77,8 +43,48 @@ class XCLient(discord.Client):
                     if s["ID"] == ID and str(s["server_id"]) == str(guild.id):
                         await chan.send("Ваш ранг равен: " + str(ranks[i]["rank"]))
                     else:
-                        print()
                         print("error")
+
+        else:
+
+            for i, s in enumerate(ranks):
+                print(s['ID'], ' == ', ID, s['ID'] == ID)
+                if s["ID"] == ID and s["server_id"] == str(guild.id) and not args.startswith("!"):
+                    print(s)
+                    print(ranks[i]['rank'])
+                    ranks[i]['rank'] += 1
+                    print(ranks[i]['rank'])
+                    k = 1
+            if k == 0:
+                ranks.append(
+                    {"ID": ID, 'name': user_name, 'rank': 1, "server_name": str(guild.name),
+                     "server_id": str(guild.id), "member_count": str(guild.member_count)})
+            f = open("dataranks.py", "r+")
+            f.write('r = [\n')
+            for i in ranks:
+                f.write(f'{str(i)},\n')
+            f.write(']')
+            f.close()
+            for s in ranks:
+                print(s)
+
+            for x in mat:
+                if x in args.lower():
+                    await chan.send("не матерись")
+                    time.sleep(2)
+                    await discord.Guild.ban(guild, user=user, reason="Ты плахая папапо", delete_message_days=1)
+                    embed = discord.Embed(color=0xFF7C00,
+                                          title="Пользователь " + f"@{user} был забанен на 24 часа.Причана - маты")
+                    embed.set_image(
+                        url="https://cdn.discordapp.com/attachments/768082564019126292/782184876102254592/489d22e04e9ea5f8.png")
+                    await chan.send(embed=embed)
+                    time.sleep(86400)
+                    embed = discord.Embed(color=0xFF7C00, title="Пользователь " + f"@{user} Разбанен.Возврощайся")
+                    embed.set_image(
+                        url="https://cdn.discordapp.com/attachments/768082564019126292/782222100780810250/2017-01-06-11-04-1982b2d7e556bdb895daece3edfa0789.jpg")
+                    await chan.send(embed=embed)
+                    await discord.Guild.unban(guild, user=user, reason="Ты плахая папапо")
+                    return
 
 
 if __name__ == '__main__' or True:
