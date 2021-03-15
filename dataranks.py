ed476b0d81a680d9202a79dfd1e9cff7ac1a3a71
noverd <71782778+noverd@users.noreply.github.com>
2021-03-15 15:54:35 +0300

    Файл с рангами(количеством сообщений)
    Изменение названий

--- dataranks.py
+++ dataranks.py
@@ -1,4 +1,4 @@
-r = [
+rank = [
 {'ID': 788448627898056706, 'name': 'RankXerBot#8987', 'rank': 3, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
 {'ID': 715483370515333200, 'name': 'Lexa#3742', 'rank': 8, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
 {'ID': 705716430213218404, 'name': 'gagarinten#5622', 'rank': 20, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
