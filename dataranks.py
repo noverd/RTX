9cac4701feadf4afeaa22fe39bd969c2ac766346
alex <dasfasru@gmail.com>
2021-05-10 16:53:26 +0300

    Бот немного доделан

--- dataranks.py
+++ dataranks.py
@@ -1,10 +1,5 @@
 # coding: utf8
 r = [
-{'ID': 788448627898056706, 'name': 'RankXerBot#8987', 'rank': 67, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
-{'ID': 715483370515333200, 'name': 'Lexa#3742', 'rank': 8, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
-{'ID': 705716430213218404, 'name': 'gagarinten#5622', 'rank': 106, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
-{'ID': 715483370515333200, 'name': 'Lexa#3742', 'rank': 7, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '6'},
-{'ID': 788448627898056706, 'name': 'RankXerBot#8987', 'rank': 3, 'server_name': '������ MadG', 'server_id': '790922688340033547', 'member_count': '2'},
-{'ID': 696431485757947966, 'name': 'MadG#6878', 'rank': 3, 'server_name': '������ MadG', 'server_id': '790922688340033547', 'member_count': '2'},
-{'ID': 720879793645551697, 'name': 'atom#9052', 'rank': 5164, 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '8'},
+{'ID': 720879793645551697, 'name': 'atom#9052', 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '8', 'cash': 224},
+{'ID': 788448627898056706, 'name': 'RankXerBot#8987', 'server_name': 'All it', 'server_id': '758312557814480896', 'member_count': '8', 0: 0},
 ]
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import discord
-
-comm = ["!rank"]
-import time
+from random import randint, choice
 from dataranks import r as ranks
-
-TOKEN = 'Nzg4NDQ4NjI3ODk4MDU2NzA2.X9jqAQ.IvKSnF5Ry_DU9wyoAf94ZSj75pM'
-
+import datetime
+buy_item = ["Доступ к галерее"]
+buy_list = ["!buy Доступ к галерее"]
+buy_cash = ["1000"]
+buy_descriptor = ["Роль для доступа в галерею"]
+TOKEN = 'Nzg4NDQ4NjI3ODk4MDU2NzA2.X9jqAQ.-LDCes1S2ex0yInnWWj0D_5sBvs'
+works = ["Вы продали себя в рабство на 30 лет.", "Вы устроились тестировщиком лекарств. У вас теперь 3 руки",
+         "Вы стали горничной и работали 95. Но за это время инфляция...",
+         "Вы ушли в армию.Вы заработали там 5.900.156 :dollar: .  Но расходы на лечение..."]
 mat = ("сук", 'xуй', "пидр", "бля")
 
 
@@ -15,7 +19,7 @@ class XCLient(discord.Client):
         print(f"ID bot:{self.user.id}")
 
     async def on_message(self, message):
-        global cash
+        global cash, buy_item
         user_name = str(message.author)
         user = message.author
         ID = user.id
@@ -43,71 +47,64 @@ class XCLient(discord.Client):
             print(f"Тело команды: {body}")
             print(f"Аргменты {arg}")
             del arg[0]
-            if body == "rank":
-                for i, s in enumerate(ranks):
-                    if s["ID"] == ID and str(s["server_id"]) == str(guild.id) and kos == 0:
-                        await chan.send("Ваш ранг равен: " + str(ranks[i]["rank"]))
-                        print("ранг найден")
-                        kos = 1
-                        break
-                    else:continue
-                if kos == 0:
-                    f = open("dataranks.py","w")
-                    f.close()
-                    ranks.append(
-                        {"ID": ID, 'name': user_name, 'rank': len(args), "server_name": str(guild.name),
-                         "server_id": str(guild.id), "member_count": str(guild.member_count)})
-                    f = open("dataranks.py", "r+")
-                    f.write('# coding: utf8\n')
-                    f.write('r = [\n')
-                    for i in ranks:
-                        f.write(f'{str(i)},\n')
-                    f.write(']')
-                    f.close()
-                    for s in ranks:
-                        print(s)
-            elif body == "buy":
+            if body == "buy":
                 buy_arg = arg[0]
-                for i,x in enumerate(arg):
+                for i, x in enumerate(arg):
                     print()
                     if i == 0:
                         print("cont")
                         continue
-                    buy_arg = buy_arg+" "+x
+                    buy_arg = buy_arg + " " + x
                     print("add")
                 print(buy_arg)
                 print("Команда !buy")
                 for i, s in enumerate(ranks):
                     if s["ID"] == ID and s["server_id"] == str(guild.id):
-                        print(ranks[i]["rank"])
-                        cash = int(ranks[i]["rank"])
+                        cash = int(ranks[i]["cash"])
                         print(cash)
                 if buy_arg == "Доступ к галерее" and cash > 9999:
                     print(message.author.guild.name)
                     role = discord.utils.get(message.guild.roles, name="Доступ в Галерею")
                     await user.add_roles(role)
                 elif buy_arg == "Доступ к галерее" and cash < 10000:
-                    await chan.send("У вас не достаточно ранга!")
+                    await chan.send("У вас не достаточно денег")
                 else:
-                    print("error")
+                    if buy_arg == "list":
+                        buy_item_list = ""
+                        for i, x in enumerate(buy_item):
+                            buy_item_list = buy_item_list + "`" + buy_item[i] + "` - " + buy_descriptor[
+                                i] + ". Цена - " + buy_cash[i] + " :dollar:\n"
+                        print(buy_item_list)
+                        liste = discord.Embed(title="Список вещей для покупки",
+                                              description=f"Вот что вы можете купить:\n{buy_item_list}",color=0x0beb10)
+                        await chan.send(embed=liste)
+            elif body == "work":
+                work = randint(10, 50)
+                for i, s in enumerate(ranks):
+                    if s["ID"] == ID and s["server_id"] == str(guild.id):
+                        ranks[i]["cash"] += work
+                worke = discord.Embed(title=":dollar: Работа :dollar:", color=0x0beb10,
+                                      description=f"{choice(works)}\nВы заработали {work} :dollar: \n Вы можете узнать свой баланс - `!balance`\n Первести деньги(Не реализованно) - `!give`\nКупить что-то - `!buy`(Посмотреть что купить - `!buy list`)")
+                await chan.send(embed=worke)
+            elif body == "balance":
+                for i, s in enumerate(ranks):
+                    if s["ID"] == ID and s["server_id"] == str(guild.id):
+                        balance = discord.Embed(title=":dollar: Твой баланс :dollar:",
+                                                description=f"Твой баланс: {ranks[i]['cash']} :dollar:\nЗаработать деньги - `!work`\n Первести деньги(Не реализованно) - `!give`\nКупить что-то - `!buy`(Посмотреть что купить - `!buy list`)",
+                                                color=0x0beb10)
+                        await chan.send(embed=balance)
+
 
 
         else:
-            f = open("dataranks.py", "w")
-            f.close()
             for i, s in enumerate(ranks):
                 print(s['ID'], ' == ', ID, s['ID'] == ID)
                 if s["ID"] == ID and s["server_id"] == str(guild.id) and not args.startswith("!"):
-                    print(s)
-                    print(ranks[i]['rank'])
-                    ranks[i]['rank'] += len(args)
-                    print(ranks[i]['rank'])
                     k = 1
             if k == 0:
-
                 ranks.append(
-                    {"ID": ID, 'name': user_name, 'rank': len(args), "server_name": str(guild.name),
-                     "server_id": str(guild.id), "member_count": str(guild.member_count)})
+                    {"ID": ID, 'name': user_name, "server_name": str(guild.name),
+                     "server_id": str(guild.id), "member_count": str(guild.member_count), cash: 0})
             f = open("dataranks.py", "r+")
             f.write('# coding: utf8\n')
             f.write('r = [\n')
@@ -117,24 +114,6 @@ class XCLient(discord.Client):
             f.close()
             for s in ranks:
                 print(s)
-
-            for x in mat:
-                if x in args.lower():
-                    await chan.send("не матерись")
-                    time.sleep(2)
-                    await discord.Guild.ban(guild, user=user, reason="Ты плахая папапо", delete_message_days=1)
-                    embed = discord.Embed(color=0xFF7C00,
-                                          title="Пользователь " + f"@{user} был забанен на 24 часа.Причана - маты")
-                    embed.set_image(
-                        url="https://cdn.discordapp.com/attachments/768082564019126292/782184876102254592/489d22e04e9ea5f8.png")
-                    await chan.send(embed=embed)
-                    time.sleep(86400)
-                    embed = discord.Embed(color=0xFF7C00, title="Пользователь " + f"@{user} Разбанен.Возврощайся")
-                    embed.set_image(
-                        url="https://cdn.discordapp.com/attachments/768082564019126292/782222100780810250/2017-01-06-11-04-1982b2d7e556bdb895daece3edfa0789.jpg")
-                    await chan.send(embed=embed)
-                    await discord.Guild.unban(guild, user=user, reason="Ты плахая папапо")
-                    return
 
 
 if __name__ == '__main__' or True:
