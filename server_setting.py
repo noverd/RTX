40702da2f1db50ff6c63bab68289684cb3ea201c
alex <dasfasru@gmail.com>
2021-05-13 14:06:37 +0300

    Бот немного доделан

--- server_setting.py
+++ server_setting.py
@@ -1,5 +1,5 @@
 # coding: utf8
 sc = [
 {'id': 0, 'name': 'server_name', 'prefix': '!', 'permission_shop': 'ADMINISTRATOR', 'permission_set_prefix': 'ADMINISTRATOR', 'set_permission': 'ADMINISTRATOR', 'shop_list': [{'name_item': [{'price': 999999999999999, 'descriptor': 'Descriptor\nBla-Bla-Bla'}]}]},
-{'id': 840543062761799690, 'name': 'Foo Bot Studio', 'prefix': 'f', 'permission_shop': 'ADMINISTRATOR', 'permission_set_prefix': 'ADMINISTRATOR', 'shop_list': {}},
+{'id': 840543062761799690, 'name': 'Foo Bot Studio', 'prefix': '!', 'permission_shop': 'ADMINISTRATOR', 'permission_set_prefix': 'ADMINISTRATOR', 'shop_list': {}},
 ]
